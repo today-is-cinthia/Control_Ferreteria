af7e7ba81bae3ba7689bb9471b125314526ad883
today-is-cinthia <87044276+today-is-cinthia@users.noreply.github.com>
2022-10-25 20:26:34 -0600

    Add files via upload

---
+++ FiltrarClientes.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ferreteria
+{
+    public partial class VerMateriales : Form
+    {
+        bool agregar = false;
+        string estado = "s";
+        float precio_gramo = 0, precioMaterial = 0, existencia = 0, nuevaExistencia = 0;
+
+        private bool existenciaBool = false;
+        /*Crea la base de datos global*/
+        //BaseDeDatos bd = new BaseDeDatos();
+        /*datatable global en private para leer los datos*/
+        private DataTable dt = new DataTable();
+
+        public VerMateriales()
+        {
+            InitializeComponent();
+            //dgvVerMateriales.DataSource = bd.SelectDataTable("select nombre_material Nombre, marca Marca, precio_material Precio_kg, existencia Existencia, estado Estado  from Material ");
+            todo_falso();
+        }
+
+        private async void btnActualizarMaterial_Click(object sender, EventArgs e)
+        {
+           /* nuevaExistencia = float.Parse(txtbVerExistenciaMaterial.Text);
+            if (txtbVerNomMaterial.Text == "" || txtbVerMarcaMaterial.Text == "" || txtbVerPreciomaterial.Text == "" || txtbVerExistenciaMaterial.Text == "")
+            {
+                errorActualizarMaterial.SetError(btnActualizarMaterial, "Uno o varios campos aun no llenos");
+            }
+            else
+            {
+                if (checkModEstadoMaterial.Checked == false)
+                {
+                    estado = "n";
+                }
+                else
+                {
+                    estado = "s";
+                }
+                if (txtbVerExistenciaMaterial.Text == "")
+                {
+                    errorPrecio.SetError(txtbVerPreciomaterial, "El precio no puede ser cero o nulo");
+                }
+                else
+                {
+                    precioMaterial = float.Parse(txtbVerPreciomaterial.Text);
+                    if (precioMaterial == 0)
+                    {
+                        errorPrecio.SetError(txtbVerPreciomaterial,"El precio no puede ser cero o nolo");
+                    }
+                    else
+                    {
+                        precio_gramo = precioMaterial / 1000;
+                        errorActualizarMaterial.SetError(btnActualizarMaterial, "");
+                        string nombreValidarMaterial = (string)dgvVerMateriales.CurrentRow.Cells["Nombre"].Value;
+                        string Cadena = "UPDATE material SET nombre_material = '" + txtbVerNomMaterial.Text + "', marca = '" + txtbVerMarcaMaterial.Text + "', precio_gramo = " + precio_gramo + ", precio_material = " + precioMaterial + ", existencia = " + nuevaExistencia + ", estado = '" + estado + "'  where nombre_material = '" + nombreValidarMaterial + "';";
+
+                        if (!string.IsNullOrEmpty(Cadena))// quitar en caso de no querer insertar un producto a la base de datos
+                        {
+                            if (!bd.executecommand(Cadena))
+                            {
+                                MessageBox.Show("Error al actualizar Material");
+                            }
+                            else
+                            {
+                                MessageBox.Show("Material actualizado con exito");
+                                errornombre.SetError(txtVerNombreMaterial, "");
+                                errormarca.SetError(txtbVerMarcaMaterial, "");
+                                errorexistencia.SetError(txtVerExistenciaMaterial, "");
+                                errorPrecio.SetError(txtbVerPreciomaterial, "");
+                            }
+
+                        }
+                        agregar = true;
+                        dgvVerMateriales.DataSource = bd.SelectDataTable("select nombre_material Nombre, marca Marca, precio_material Precio_kg, existencia Existencia, estado Estado  from Material ");
+                        txtbVerNomMaterial.Text = "";
+                        txtbVerMarcaMaterial.Text = "";
+                        txtbVerExistenciaMaterial.Text = "";
+                        txtbVerPreciomaterial.Text = "";
+                        todo_falso();
+                    }
+                    
+                } 
+            }*/
+            
+        }
+
+        private void valirCampos(object sender, MouseEventArgs e)
+        {
+            /*if (!agregar)
+            {
+                if (txtbVerNomMaterial.Text == "")
+                {
+                    errornombre.SetError(txtbVerNomMaterial, "Debe llenar este campo");
+                }
+                if (txtbVerMarcaMaterial.Text == "")
+                {
+                    errormarca.SetError(txtbVerMarcaMaterial, "Debe llenar este campo");
+                }
+                if (txtbVerExistenciaMaterial.Text == "")
+                {
+                    errorexistencia.SetError(txtbVerExistenciaMaterial, "Debe llenar este campo");
+                }
+                if (txtbVerPreciomaterial.Text == "")
+                {
+                    errorPrecio.SetError(txtbVerPreciomaterial, "Debe llenar este campo");
+                }
+            }*/
+            agregar = false;
+        }
+
+        private void validar(object sender, EventArgs e)
+        {
+            /*if (txtbVerNomMaterial.Text != "")
+            {
+                errornombre.SetError(txtVerNombreMaterial, "");
+            }
+            if (txtbVerMarcaMaterial.Text != "")
+            {
+                errormarca.SetError(txtbVerMarcaMaterial, "");
+            }
+            if (txtVerExistenciaMaterial.Text != "")
+            {
+                errorexistencia.SetError(txtbVerExistenciaMaterial, "");
+            }
+            if (txtbVerPreciomaterial.Text != "")
+            {
+                errorPrecio.SetError(txtbVerPreciomaterial, "");
+            }*/
+        }
+
+       
+        private void todo_falso()
+        {
+           /* txtbVerNomMaterial.Enabled = false;
+            txtbVerPreciomaterial.Enabled = false;
+            checkModEstadoMaterial.Enabled = false;
+            txtbVerMarcaMaterial.Enabled = false;
+            txtbVerExistenciaMaterial.Enabled = false;*/
+        }
+
+        private void dgvVerMateriales_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            
+           /* txtbVerNomMaterial.Text = (string)dgvVerMateriales.CurrentRow.Cells["Nombre"].Value;
+            txtbVerMarcaMaterial.Text = (string)dgvVerMateriales.CurrentRow.Cells["Marca"].Value;
+            txtbVerPreciomaterial.Text = ((float)dgvVerMateriales.CurrentRow.Cells["Precio_kg"].Value).ToString();
+            txtbVerExistenciaMaterial.Text = ((float)dgvVerMateriales.CurrentRow.Cells["Existencia"].Value).ToString();
+            string estado = ((string)dgvVerMateriales.CurrentRow.Cells["Estado"].Value);
+            if (estado == "s")
+            {
+                checkModEstadoMaterial.Checked = true;
+            }
+            else
+            {
+                checkModEstadoMaterial.Checked = false;
+            }
+
+            txtbVerNomMaterial.Enabled = true;
+            txtbVerPreciomaterial.Enabled = true;
+            checkModEstadoMaterial.Enabled = true;
+            txtbVerMarcaMaterial.Enabled = true;
+            txtbVerExistenciaMaterial.Enabled = true;
+
+            precioMaterial = float.Parse(txtbVerPreciomaterial.Text);
+            nuevaExistencia = float.Parse(this.txtbVerExistenciaMaterial.Text);
+           */
+        }
+
+        private void Buscar_TextChanged(object sender, EventArgs e)
+        {
+            var cliente = Declaraciones.Clientes;
+            string dato = Buscar.Text;
+            if (dato == "")
+            {
+                var Consulta = cliente.Select(x => x);
+                //  buscarPorid = "select nombre_producto, id_categoria, id_material, peso_producto, total from Productos ORDER BY id_productos ASC";
+            }
+            else
+            {
+                var Consulta = cliente.Where(x => x.Nombre.Contains(dato)).Select(x => x);
+                // buscarPorid = "select nombre_material Nombre, marca Marca, precio_material Precio_kg, existencia Existencia, estado Estado  from Material  where nombre_material LIKE '%" + dato + "%' ORDER BY nombre_material ASC";
+                // buscarPorid = "select nombre_producto, id_categoria, id_material, peso_producto, total from Productos where nombre_producto LIKE '%" + dato + "%' ORDER BY nombre_producto ASC";
+            }
+
+           // dgvVerMateriales.DataSource = bd.SelectDataTable(buscarPorid);
+        }
+
+        private void soloTexto(object sender, KeyPressEventArgs e)
+        {
+            if (!(char.IsLetter(e.KeyChar)) && (e.KeyChar != (char)Keys.Back) && !(char.IsWhiteSpace(e.KeyChar)) && !(char.IsDigit(e.KeyChar)))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void soloNum(object sender, KeyPressEventArgs e)
+        {
+            if (!(char.IsControl(e.KeyChar)) && !(char.IsDigit(e.KeyChar)) && (e.KeyChar != '.'))
+            {
+                e.Handled = true;
+            }
+
+            if ((e.KeyChar == '.') && ((sender as TextBox).Text.IndexOf('.') > -1))
+            {
+                e.Handled = true;
+            }
+        }
+
+    }
+}
---
+++ FiltrarClientes.designer.cs
@@ -0,0 +1,222 @@
+﻿namespace ferreteria
+{
+    partial class VerMateriales
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        /*protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+        */
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.btnActualizarMaterial = new System.Windows.Forms.Button();
+            this.txtAdvertencia = new System.Windows.Forms.Label();
+            this.dgvVerMateriales = new System.Windows.Forms.DataGridView();
+            this.Buscar = new System.Windows.Forms.TextBox();
+            this.txtVerNombreMaterial = new System.Windows.Forms.Label();
+            this.errorActualizarMaterial = new System.Windows.Forms.ErrorProvider(this.components);
+            this.errornombre = new System.Windows.Forms.ErrorProvider(this.components);
+            this.errormarca = new System.Windows.Forms.ErrorProvider(this.components);
+            this.errorexistencia = new System.Windows.Forms.ErrorProvider(this.components);
+            this.errorPrecio = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvVerMateriales)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorActualizarMaterial)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errornombre)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errormarca)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorexistencia)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorPrecio)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnActualizarMaterial
+            // 
+            this.btnActualizarMaterial.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnActualizarMaterial.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(172)))), ((int)(((byte)(126)))), ((int)(((byte)(241)))));
+            this.btnActualizarMaterial.FlatAppearance.BorderSize = 0;
+            this.btnActualizarMaterial.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnActualizarMaterial.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnActualizarMaterial.ForeColor = System.Drawing.Color.Black;
+            this.btnActualizarMaterial.Location = new System.Drawing.Point(303, 145);
+            this.btnActualizarMaterial.Margin = new System.Windows.Forms.Padding(2);
+            this.btnActualizarMaterial.Name = "btnActualizarMaterial";
+            this.btnActualizarMaterial.Size = new System.Drawing.Size(85, 33);
+            this.btnActualizarMaterial.TabIndex = 20;
+            this.btnActualizarMaterial.Text = "Hecho";
+            this.btnActualizarMaterial.UseVisualStyleBackColor = false;
+            this.btnActualizarMaterial.Click += new System.EventHandler(this.btnActualizarMaterial_Click);
+            this.btnActualizarMaterial.MouseClick += new System.Windows.Forms.MouseEventHandler(this.valirCampos);
+            // 
+            // txtAdvertencia
+            // 
+            this.txtAdvertencia.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.txtAdvertencia.AutoSize = true;
+            this.txtAdvertencia.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAdvertencia.ForeColor = System.Drawing.Color.White;
+            this.txtAdvertencia.Location = new System.Drawing.Point(398, 344);
+            this.txtAdvertencia.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.txtAdvertencia.Name = "txtAdvertencia";
+            this.txtAdvertencia.Size = new System.Drawing.Size(0, 13);
+            this.txtAdvertencia.TabIndex = 22;
+            // 
+            // dgvVerMateriales
+            // 
+            this.dgvVerMateriales.AllowUserToAddRows = false;
+            this.dgvVerMateriales.AllowUserToOrderColumns = true;
+            this.dgvVerMateriales.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgvVerMateriales.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvVerMateriales.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dgvVerMateriales.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(66)))), ((int)(((byte)(91)))));
+            this.dgvVerMateriales.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(66)))), ((int)(((byte)(91)))));
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.Color.DarkSlateGray;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvVerMateriales.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.dgvVerMateriales.ColumnHeadersHeight = 30;
+            this.dgvVerMateriales.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle2.BackColor = System.Drawing.Color.Gray;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.Color.Black;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.Color.DarkSlateGray;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvVerMateriales.DefaultCellStyle = dataGridViewCellStyle2;
+            this.dgvVerMateriales.EnableHeadersVisualStyles = false;
+            this.dgvVerMateriales.Location = new System.Drawing.Point(27, 210);
+            this.dgvVerMateriales.Name = "dgvVerMateriales";
+            this.dgvVerMateriales.ReadOnly = true;
+            this.dgvVerMateriales.RowHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle3.BackColor = System.Drawing.Color.Gray;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle3.ForeColor = System.Drawing.Color.Black;
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.Color.DarkSlateGray;
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dgvVerMateriales.RowHeadersDefaultCellStyle = dataGridViewCellStyle3;
+            this.dgvVerMateriales.RowHeadersVisible = false;
+            dataGridViewCellStyle4.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle4.BackColor = System.Drawing.Color.Gray;
+            dataGridViewCellStyle4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle4.ForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle4.SelectionBackColor = System.Drawing.Color.Teal;
+            dataGridViewCellStyle4.SelectionForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle4.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvVerMateriales.RowsDefaultCellStyle = dataGridViewCellStyle4;
+            this.dgvVerMateriales.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvVerMateriales.Size = new System.Drawing.Size(650, 214);
+            this.dgvVerMateriales.TabIndex = 37;
+            this.dgvVerMateriales.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvVerMateriales_CellClick);
+            // 
+            // Buscar
+            // 
+            this.Buscar.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.Buscar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Buscar.Location = new System.Drawing.Point(236, 87);
+            this.Buscar.Multiline = true;
+            this.Buscar.Name = "Buscar";
+            this.Buscar.Size = new System.Drawing.Size(215, 42);
+            this.Buscar.TabIndex = 40;
+            this.Buscar.TextChanged += new System.EventHandler(this.Buscar_TextChanged);
+            this.Buscar.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.soloTexto);
+            // 
+            // txtVerNombreMaterial
+            // 
+            this.txtVerNombreMaterial.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.txtVerNombreMaterial.AutoSize = true;
+            this.txtVerNombreMaterial.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtVerNombreMaterial.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(30)))), ((int)(((byte)(68)))));
+            this.txtVerNombreMaterial.Location = new System.Drawing.Point(281, 38);
+            this.txtVerNombreMaterial.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.txtVerNombreMaterial.Name = "txtVerNombreMaterial";
+            this.txtVerNombreMaterial.Size = new System.Drawing.Size(117, 20);
+            this.txtVerNombreMaterial.TabIndex = 39;
+            this.txtVerNombreMaterial.Text = "Filtrar Cliente";
+            // 
+            // errorActualizarMaterial
+            // 
+            this.errorActualizarMaterial.ContainerControl = this;
+            // 
+            // errornombre
+            // 
+            this.errornombre.ContainerControl = this;
+            // 
+            // errormarca
+            // 
+            this.errormarca.ContainerControl = this;
+            // 
+            // errorexistencia
+            // 
+            this.errorexistencia.ContainerControl = this;
+            // 
+            // errorPrecio
+            // 
+            this.errorPrecio.ContainerControl = this;
+            // 
+            // VerMateriales
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Silver;
+            this.ClientSize = new System.Drawing.Size(714, 532);
+            this.Controls.Add(this.Buscar);
+            this.Controls.Add(this.txtVerNombreMaterial);
+            this.Controls.Add(this.dgvVerMateriales);
+            this.Controls.Add(this.txtAdvertencia);
+            this.Controls.Add(this.btnActualizarMaterial);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "VerMateriales";
+            this.Text = "VerMateriales";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvVerMateriales)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorActualizarMaterial)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errornombre)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errormarca)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorexistencia)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorPrecio)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnActualizarMaterial;
+        private System.Windows.Forms.Label txtAdvertencia;
+        private System.Windows.Forms.DataGridView dgvVerMateriales;
+        private System.Windows.Forms.TextBox Buscar;
+        private System.Windows.Forms.Label txtVerNombreMaterial;
+        private System.Windows.Forms.ErrorProvider errorActualizarMaterial;
+        private System.Windows.Forms.ErrorProvider errornombre;
+        private System.Windows.Forms.ErrorProvider errormarca;
+        private System.Windows.Forms.ErrorProvider errorexistencia;
+        private System.Windows.Forms.ErrorProvider errorPrecio;
+    }
+}
